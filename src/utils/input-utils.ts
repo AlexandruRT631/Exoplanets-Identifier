cbfdb0656f823754eab5aade863a7749eaf99314
KriSSu09 <cristi.serban09@yahoo.com>
2025-10-04 20:27:59 +0300

    remove limit

--- src/utils/input-utils.ts
+++ src/utils/input-utils.ts
@@ -2,22 +2,18 @@ export type InputProps = {
   period_d: number | undefined;
   sigma_period_d: number | undefined;
   asym_period_d: number | undefined;
-  is_limit_period_d: number | undefined;
 
   dur_hr: number | undefined;
   sigma_dur_hr: number | undefined;
   asym_dur_hr: number | undefined;
-  is_limit_dur_hr: number | undefined;
 
   depth_ppm: number | undefined;
   sigma_depth_ppm: number | undefined;
   asym_depth_ppm: number | undefined;
-  is_limit_depth_ppm: number | undefined;
 
   ror: number | undefined;
   sigma_ror: number | undefined;
   asym_ror: number | undefined;
-  is_limit_ror: number | undefined;
 
   incl_deg: number | undefined;
   a_over_r: number | undefined;
@@ -28,38 +24,31 @@ export type InputProps = {
   prad_re: number | undefined;
   sigma_prad_re: number | undefined;
   asym_prad_re: number | undefined;
-  is_limit_prad_re: number | undefined;
 
   insol_ef: number | undefined;
   teq_k: number | undefined;
   sigma_teq_k: number | undefined;
   asym_teq_k: number | undefined;
-  is_limit_teq_k: number | undefined;
 
   teff_k: number | undefined;
   sigma_teff_k: number | undefined;
   asym_teff_k: number | undefined;
-  is_limit_teff_k: number | undefined;
 
   logg_cgs: number | undefined;
   sigma_logg_cgs: number | undefined;
   asym_logg_cgs: number | undefined;
-  is_limit_logg_cgs: number | undefined;
 
   met_dex: number | undefined;
   sigma_met_dex: number | undefined;
   asym_met_dex: number | undefined;
-  is_limit_met_dex: number | undefined;
 
   rstar_rs: number | undefined;
   sigma_rstar_rs: number | undefined;
   asym_rstar_rs: number | undefined;
-  is_limit_rstar_rs: number | undefined;
 
   mstar_ms: number | undefined;
   sigma_mstar_ms: number | undefined;
   asym_mstar_ms: number | undefined;
-  is_limit_mstar_ms: number | undefined;
 
   rho_cgs: number | undefined;
 
@@ -73,28 +62,24 @@ export const fieldsNameGroupsDictionary: Record<string, (keyof InputProps)[]> = 
     "period_d",
     "sigma_period_d",
     "asym_period_d",
-    "is_limit_period_d",
   ],
 
   "Transit Duration": [
     "dur_hr",
     "sigma_dur_hr",
     "asym_dur_hr",
-    "is_limit_dur_hr",
   ],
 
   "Transit Depth": [
     "depth_ppm",
     "sigma_depth_ppm",
     "asym_depth_ppm",
-    "is_limit_depth_ppm",
   ],
 
   "Radius Ratio": [
     "ror",
     "sigma_ror",
     "asym_ror",
-    "is_limit_ror",
   ],
 
   "Orbital / Transit Geometry": [
@@ -109,7 +94,6 @@ export const fieldsNameGroupsDictionary: Record<string, (keyof InputProps)[]> = 
     "prad_re",
     "sigma_prad_re",
     "asym_prad_re",
-    "is_limit_prad_re",
   ],
 
   "Irradiation & Temperature": [
@@ -117,42 +101,36 @@ export const fieldsNameGroupsDictionary: Record<string, (keyof InputProps)[]> = 
     "teq_k",
     "sigma_teq_k",
     "asym_teq_k",
-    "is_limit_teq_k",
   ],
 
   "Stellar Temperature": [
     "teff_k",
     "sigma_teff_k",
     "asym_teff_k",
-    "is_limit_teff_k",
   ],
 
   "Stellar Gravity": [
     "logg_cgs",
     "sigma_logg_cgs",
     "asym_logg_cgs",
-    "is_limit_logg_cgs",
   ],
 
   "Stellar Metallicity": [
     "met_dex",
     "sigma_met_dex",
     "asym_met_dex",
-    "is_limit_met_dex",
   ],
 
   "Stellar Radius": [
     "rstar_rs",
     "sigma_rstar_rs",
     "asym_rstar_rs",
-    "is_limit_rstar_rs",
   ],
 
   "Stellar Mass": [
     "mstar_ms",
     "sigma_mstar_ms",
     "asym_mstar_ms",
-    "is_limit_mstar_ms",
   ],
 
   "Stellar Density": [
@@ -170,22 +148,18 @@ export const fieldsNameDictionary: Record<keyof InputProps, string> = {
   period_d: "Orbital period (days)",
   sigma_period_d: "σ(Orbital period)",
   asym_period_d: "Asymmetry (Orbital period)",
-  is_limit_period_d: "Is limit: Orbital period",
 
   dur_hr: "Transit duration (hours)",
   sigma_dur_hr: "σ(Transit duration)",
   asym_dur_hr: "Asymmetry (Transit duration)",
-  is_limit_dur_hr: "Is limit: Transit duration",
 
   depth_ppm: "Transit depth (ppm)",
   sigma_depth_ppm: "σ(Transit depth)",
   asym_depth_ppm: "Asymmetry (Transit depth)",
-  is_limit_depth_ppm: "Is limit: Transit depth",
 
   ror: "Planet–star radius ratio (Rp/R*)",
   sigma_ror: "σ(Rp/R*)",
   asym_ror: "Asymmetry (Rp/R*)",
-  is_limit_ror: "Is limit: Rp/R*",
 
   incl_deg: "Orbital inclination (deg)",
   a_over_r: "Scaled semi-major axis (a/R*)",
@@ -196,38 +170,31 @@ export const fieldsNameDictionary: Record<keyof InputProps, string> = {
   prad_re: "Planet radius (Earth radii)",
   sigma_prad_re: "σ(Planet radius)",
   asym_prad_re: "Asymmetry (Planet radius)",
-  is_limit_prad_re: "Is limit: Planet radius",
 
   insol_ef: "Insolation flux (Earth flux)",
   teq_k: "Equilibrium temperature (K)",
   sigma_teq_k: "σ(Equilibrium temperature)",
   asym_teq_k: "Asymmetry (Equilibrium temperature)",
-  is_limit_teq_k: "Is limit: Equilibrium temperature",
 
   teff_k: "Stellar effective temperature (K)",
   sigma_teff_k: "σ(Teff)",
   asym_teff_k: "Asymmetry (Teff)",
-  is_limit_teff_k: "Is limit: Teff",
 
   logg_cgs: "Stellar surface gravity (log10 cm/s²)",
   sigma_logg_cgs: "σ(log g)",
   asym_logg_cgs: "Asymmetry (log g)",
-  is_limit_logg_cgs: "Is limit: log g",
 
   met_dex: "Stellar metallicity [dex]",
   sigma_met_dex: "σ([M/H])",
   asym_met_dex: "Asymmetry ([M/H])",
-  is_limit_met_dex: "Is limit: [M/H]",
 
   rstar_rs: "Stellar radius (solar radii)",
   sigma_rstar_rs: "σ(Stellar radius)",
   asym_rstar_rs: "Asymmetry (Stellar radius)",
-  is_limit_rstar_rs: "Is limit: Stellar radius",
 
   mstar_ms: "Stellar mass (solar masses)",
   sigma_mstar_ms: "σ(Stellar mass)",
   asym_mstar_ms: "Asymmetry (Stellar mass)",
-  is_limit_mstar_ms: "Is limit: Stellar mass",
 
   rho_cgs: "Stellar density (g/cm³)",
 
